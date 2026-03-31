45d83dc5bd7ad1c937d79b6e3a85bb67c66222e8
Sun Ruiheng <sunruiheng2001@gmail.com>
2026-03-31 20:38:45 +0800

    change to 3B data and src

--- mainpy_sent.py
+++ mainpy_sent.py
@@ -5,10 +5,10 @@ from vllm_angular_steering import AngularSteering
 def main():
     os.environ["VLLM_ALLOW_INSECURE_SERIALIZATION"] = "1"
     # Initialize vLLM (enforce_eager=True is REQUIRED)
-    llm = LLM(model="Qwen/Qwen2.5-7B-Instruct", enforce_eager=True, gpu_memory_utilization=0.75)
+    llm = LLM(model="Qwen/Qwen2.5-3B-Instruct", enforce_eager=True, gpu_memory_utilization=0.75)
     # Load and apply steering (using available config file)
     steering = AngularSteering(llm)
-    steering.load_config_from_file("output/STMT/steering_config-en-max_norm_23_post-pca_0.npy")
+    steering.load_config_from_file("output/Qwen2.5-3B-Instruct/STMT/steering_config-en-max_norm_35_post-pca_0.npy")
     steering.apply_steering(target_degree=180, adaptive_mode=1)
 
     # Example prompts
